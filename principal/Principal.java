d3b23d21dda18295444111751ab65cd024385f26
Tomás <tmalavassi.r@gmail.com>
2018-09-14 22:44:27 -0600

    Prueba inicial para sockets

--- principal/Principal.java
+++ principal/Principal.java
@@ -6,7 +6,7 @@ public class Principal {
 	public static void main(String[] args) {
 		Tablero T1=new Tablero(3);
 		Linea L1=new Linea(00,11,T1);
-		T1.show2();
+		T1.show2(); //prueba para branch de sockets
 		
 	}
 }
