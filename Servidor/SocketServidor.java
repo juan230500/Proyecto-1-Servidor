c50b0b09998881c5634c2ae2897957166c364f08
Tomás <tmalavassi.r@gmail.com>
2018-09-17 14:22:08 -0700

    Metodos de serializacion y deserializacion
    Se agregan metodos de serializacion y deserializacion y otors varios, se realizan correciones

--- Servidor/SocketServidor.java
+++ Servidor/SocketServidor.java
@@ -1,38 +1,88 @@
 package Servidor;
 import java.io.*;
 import java.net.*;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import Cliente.Envio;
+import Cliente.Respuesta;
+import geo.*;
 /*
  * 
  */
 public class SocketServidor {
-	public static void main(String[] args) throws IOException{
-		ServerSocket ServidorSocket = null;
+	Socket ClienteSocket = null;
+	Socket ClienteSocket1 = null;
+	ServerSocket ServidorSocket = null;
+	ServerSocket ServidorSocket1 = null;
+	BufferedReader entrada = null;
+	BufferedReader entrada1 = null;
+	PrintWriter salida = null;
+	PrintWriter salida1 = null;
+	int n = 7779;
+	int n1 = 7780;
+	public  void start() throws IOException{
+		
 		try {
 			ServidorSocket = new ServerSocket(7777);
-		}
+			ServidorSocket1 = new ServerSocket(7778); 		
+					}
 		catch(IOException error) {
-			System.err.println("No se puede escuchar en puerto 7777");
+			System.err.println("No se puede escuchar en puerto 7777 o 7778");
 			System.exit(1);
 		}
-		Socket ClienteSocket = null;
+		
 		
 		try {
 			ClienteSocket = ServidorSocket.accept();
+			ClienteSocket1 = ServidorSocket1.accept();
 		}
 		catch(IOException error){
 			System.err.println("Falla en la conexión");
 			System.exit(1);
 		}
 		PrintWriter salida = new PrintWriter(ClienteSocket.getOutputStream(), true);
+		PrintWriter salida1 = new PrintWriter(ClienteSocket1.getOutputStream(), true);
 		BufferedReader entrada = new BufferedReader(new InputStreamReader(ClienteSocket.getInputStream()));		
-		String inputLine, outputLine;
-		while((inputLine = entrada.readLine())!=null) {
+		BufferedReader entrada1 = new BufferedReader(new InputStreamReader(ClienteSocket.getInputStream()));		
+		String inputLine, outputLine, inputLine1, outputLine1;
+		while(((inputLine = entrada.readLine())!=null)&&((inputLine1 = entrada1.readLine())!=null)) {
 			outputLine = inputLine;
+			outputLine1 = inputLine1;
 			salida.println("Fwd:"+outputLine);
-			if(outputLine.equals("salir"))
+			salida1.println("Fwd1:"+outputLine1);
+			if(outputLine.equals("salir")||outputLine1.equals("salir"))
 				break;
-			
+		
 		}
+		
+	}
+	public static String Ansout(Respuesta e)JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		String json = null;
+			json = mapper.writeValueAsString(e);
+		return json;
+	}
+	public static Respuesta Ansin(String json) throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		Respuesta a = mapper.readValue(json, Respuesta.class);
+		return a;
+	}
+	public static String Shipout(Envio e)JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		String json = null;
+			json = mapper.writeValueAsString(e);
+		return json;
+	}
+	public static Envio Shipin(String json) throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		Envio e = mapper.readValue(json, Respuesta.class);
+		return e;
+	}
+	public void close() throws IOException {
 		salida.close();
 		entrada.close();
 		ClienteSocket.close();
