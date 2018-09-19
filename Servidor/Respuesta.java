47361a0b6bbb0a51a57bedbd0f603430c06596b9
Tomás <tmalavassi.r@gmail.com>
2018-09-18 23:41:52 -0700

    Modificación clases Envio y Respuesta
    Se añaden métodos para serializar y desserializar los objetos de dichas clases y Javadoc

--- Servidor/Respuesta.java
+++ Servidor/Respuesta.java
@@ -5,6 +5,16 @@
  */
 package Servidor;
 
+import java.io.IOException;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
+
 /**
  *
  * @author reds
@@ -46,6 +56,33 @@ public class Respuesta {
     public void setYpos(int[] ypos) {
         this.ypos = ypos;
     }
-    
-    
+/**
+ * Metodo que serializa un objeto de la clase Respuesta en formato JSON como String
+ * @param e Objeto de la clase Respuesta a serializar
+ * @return String en formato JSON
+ * @throws JsonProcessingException Indicador en caso de error
+ */
+	public String Ansout(Respuesta e) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		String json = null;
+			json = mapper.writeValueAsString(e);
+		return json;
+	}
+    /**
+     * Metodo que deserializa un String con formato JSON en un objeto de la clase Respuesta
+     * @param json String en formato JSON a deserializar
+     * @throws JsonParseException Indicador en caso de error
+     * @throws JsonMappingException Indicador en caso de error
+     * @throws IOException Indicador en caso de error
+     */
+    public void Ansin(String json) throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY);
+		Respuesta a = mapper.readValue(json, Respuesta.class);
+		this.Dibujo = a.Dibujo;
+		this.espera = a.espera;
+		this.xpos = a.xpos;
+		this.ypos = a.ypos;
+}
 }
